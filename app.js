c32d1d5746df057b8de9ed6a1d096772e5850ca3
Marcus McBride <marcusmcb@gmail.com>
2020-08-03 00:08:49 -0700

    solved src replacement issue, part 1

--- app.js
+++ app.js
@@ -1,71 +1,69 @@
 // click handler to determine active drum kit
 // NOTE: this will apply to all future buttons added even if they're not kit selectors
 
-$(document).ready(function() {
-    $('button').on('click', function() {
-      $('button').removeClass('active');
-      $(this).addClass('active');
+$(document).ready(function () {
+  $("button").on("click", function () {
+    $("button").removeClass("active");
+    $(this).addClass("active");
 
-      // trigger corresponding kit change?
-      changeKit();      
-    });
+    // trigger corresponding kit change?
+    changeKit();
   });
+});
 
-  // css function for transition element
-  function removeTransition(e) {
-    if (e.propertyName !== "transform") return;
-    e.target.classList.remove("playing");
+// css function for transition element
+function removeTransition(e) {
+  if (e.propertyName !== "transform") return;
+  e.target.classList.remove("playing");
+}
+
+// function to trigger individual sounds
+function playSound(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+  if (!audio) return;
+  key.classList.add("playing");
+  audio.currentTime = 0;
+  audio.play();
+}
+
+const keys = Array.from(document.querySelectorAll(".key"));
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
+window.addEventListener("keydown", playSound);
+
+// tentative function to change drum kits
+function changeKit() {
+  // grab "active" kit to use as var in string substitution
+  let activeKit = document.querySelector(".active").id;
+  console.log(activeKit);
+  let regex = /kit_\w/gi;
+  let audioElements = document.querySelectorAll("source");
+  for (i = 0; i < audioElements.length; i++) {
+    let currentSourcePath = audioElements[i].src;
+    let newSourcePath = currentSourcePath.replace(regex, activeKit);
+    console.log(newSourcePath)
+    // const p = "sounds/kit_a/clap.wav";
+    // let x = p.replace("kit_a", "kit_b");
+    // console.log(x);
   }
+}
 
-  // function to trigger individual sounds
-  function playSound(e) {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
-    if (!audio) return;
-    key.classList.add("playing");
-    audio.currentTime = 0;
-    audio.play();
-  }
+// use replace method?
 
-  const keys = Array.from(document.querySelectorAll(".key"));
-  keys.forEach((key) =>
-    key.addEventListener("transitionend", removeTransition)
-  );
-  window.addEventListener("keydown", playSound);
-
-  // tentative function to change drum kits
-  function changeKit() {
-    
-    // grab "active" kit to use as var in string substitution
-    let activeKit = document.querySelector(".active").id
-    console.log(activeKit)
-
-    let audioElements = document.querySelectorAll("source");    
-    for (i = 0; i < audioElements.length; i++) {
-      let audioElement = audioElements[i].src
-      let audioSource = audioElement.split("sounds/")[1].split("/")[1]
-      // audioSource = audioSource[1]
-      audioSource = activeKit             
-      };    
-    }
-
-    // use replace method?
-
-    // let clap = document.getElementById('clap')
-    //  ^ one for each element in the kit
-    //
-    //  check to see which kit is marked active
-    //
-    // if kit a = active
-    //    clap.src='sounds/kit_a/clap.wav'
-    //    clap.load()
-    //
-    //  
-    //  let soundkit = document.querySelectorAll("audio");
-    //  for (i = 0; i < soundkit.length; i++) {
-    //    console.log(soundkit.src)
-    //
-    //    check for active kit & change srcs in audio elements via string interpolation
-    //  use on-click trigger function for each kit in HTML?
-    //  
-  
+// let clap = document.getElementById('clap')
+//  ^ one for each element in the kit
+//
+//  check to see which kit is marked active
+//
+// if kit a = active
+//    clap.src='sounds/kit_a/clap.wav'
+//    clap.load()
+//
+//
+//  let soundkit = document.querySelectorAll("audio");
+//  for (i = 0; i < soundkit.length; i++) {
+//    console.log(soundkit.src)
+//
+//    check for active kit & change srcs in audio elements via string interpolation
+//  use on-click trigger function for each kit in HTML?
+//
