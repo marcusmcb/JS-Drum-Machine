072260da9c35b11a462ff24efcd439c02a108b7c
Marcus McBride <marcusmcb@gmail.com>
2020-08-08 14:48:16 -0700

    added note logging into app.js

--- app.js
+++ app.js
@@ -75,6 +75,30 @@ function onMIDISuccess(midiAccess) {
       input.onmidimessage = getMIDIMessage;
   }
 
-function getMIDIMessage(midiEvent) {
-  console.log(midiEvent);
+  function noteOn(note) {
+    console.log(note)
+  }
+
+  function noteOff(note) {
+  }
+
+  function getMIDIMessage(message) {
+    console.log(message)
+    var command = message.data[0];
+    var note = message.data[1];
+    var velocity = (message.data.length > 2) ? message.data[2] : 0; // a velocity value might not be included with a noteOff command
+
+    switch (command) {
+        case 144: // noteOn
+            if (velocity > 0) {
+                noteOn(note, velocity);
+            } else {
+                noteOff(note);
+            }
+            break;
+        case 128: // noteOff
+            noteOff(note);
+            break;
+        // we could easily expand this switch statement to cover other types of commands such as controllers or sysex
+    }
 }
