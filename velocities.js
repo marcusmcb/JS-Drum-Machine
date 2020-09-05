afee1c4d255ea55d25ce1d045c883f7c3d365927
Marcus McBride <marcusmcb@gmail.com>
2020-09-05 15:02:00 -0700

    added remaining cases to velocity function

--- velocities.js
+++ velocities.js
@@ -1,6 +1,5 @@
 export function setVelocity(velocity, audio) {
-  console.log(`NOTE VELOCITY: ${velocity}`);
-  console.log(audio.volume);
+  console.log(`NOTE VELOCITY: ${velocity}`);  
   switch (velocity) {
     case 1:
       audio.volume = 0.0078;
@@ -123,265 +122,265 @@ export function setVelocity(velocity, audio) {
       audio.volume = 0.312;
       break;
     case 41:
-      audio.volume = 0;
+      audio.volume = 0.319;
       break;
     case 42:
-      audio.volume = 0;
+      audio.volume = 0.327;
       break;
     case 43:
-      audio.volume = 0;
+      audio.volume = 0.335;
       break;
     case 44:
-      audio.volume = 0;
+      audio.volume = 0.343;
       break;
     case 45:
-      audio.volume = 0;
+      audio.volume = 0.351;
       break;
     case 46:
-      audio.volume = 0;
+      audio.volume = 0.359;
       break;
     case 47:
-      audio.volume = 0;
+      audio.volume = 0.367;
       break;
     case 48:
-      audio.volume = 0;
+      audio.volume = 0.374;
       break;
     case 49:
-      audio.volume = 0;
+      audio.volume = 0.382;
       break;
     case 50:
-      audio.volume = 0;
+      audio.volume = 0.39;
       break;
     case 51:
-      audio.volume = 0;
+      audio.volume = 0.398;
       break;
     case 52:
-      audio.volume = 0;
+      audio.volume = 0.406;
       break;
     case 53:
-      audio.volume = 0;
+      audio.volume = 0.413;
       break;
     case 54:
-      audio.volume = 0;
+      audio.volume = 0.421;
       break;
     case 55:
-      audio.volume = 0;
+      audio.volume = 0.430;
       break;
     case 56:
-      audio.volume = 0;
+      audio.volume = 0.437;
       break;
     case 57:
-      audio.volume = 0;
+      audio.volume = 0.445;
       break;
     case 58:
-      audio.volume = 0;
+      audio.volume = 0.452;
       break;
     case 59:
-      audio.volume = 0;
+      audio.volume = 0.460;
       break;
     case 60:
-      audio.volume = 0;
+      audio.volume = 0.468;
       break;
     case 61:
-      audio.volume = 0;
+      audio.volume = 0.476;
       break;
     case 62:
-      audio.volume = 0;
+      audio.volume = 0.484;
       break;
     case 63:
-      audio.volume = 0;
+      audio.volume = 0.491;
       break;
     case 64:
-      audio.volume = 0;
+      audio.volume = 0.499;
       break;
     case 65:
-      audio.volume = 0;
+      audio.volume = 0.507;
       break;
     case 66:
-      audio.volume = 0;
+      audio.volume = 0.515;
       break;
     case 67:
-      audio.volume = 0;
+      audio.volume = 0.523;
       break;
     case 68:
-      audio.volume = 0;
+      audio.volume = 0.530;
       break;
     case 69:
-      audio.volume = 0;
+      audio.volume = 0.538;
       break;
     case 70:
-      audio.volume = 0;
+      audio.volume = 0.546;
       break;
     case 71:
-      audio.volume = 0;
+      audio.volume = 0.554;
       break;
     case 72:
-      audio.volume = 0;
+      audio.volume = 0.562;
       break;
     case 73:
-      audio.volume = 0;
+      audio.volume = 0.569;
       break;
     case 74:
-      audio.volume = 0;
+      audio.volume = 0.577;
       break;
     case 75:
-      audio.volume = 0;
+      audio.volume = 0.585;
       break;
     case 76:
-      audio.volume = 0;
+      audio.volume = 0.593;
       break;
     case 77:
-      audio.volume = 0;
+      audio.volume = 0.601;
       break;
     case 78:
-      audio.volume = 0;
+      audio.volume = 0.608;
       break;
     case 79:
-      audio.volume = 0;
+      audio.volume = 0.616;
       break;
     case 80:
-      audio.volume = 0;
+      audio.volume = 0.624;
       break;
     case 81:
-      audio.volume = 0;
+      audio.volume = 0.632;
       break;
     case 82:
-      audio.volume = 0;
+      audio.volume = 0.640;
       break;
     case 83:
-      audio.volume = 0;
+      audio.volume = 0.647;
       break;
     case 84:
-      audio.volume = 0;
+      audio.volume = 0.655;
       break;
     case 85:
-      audio.volume = 0;
+      audio.volume = 0.663;
       break;
     case 86:
-      audio.volume = 0;
+      audio.volume = 0.671;
       break;
     case 87:
-      audio.volume = 0;
+      audio.volume = 0.679;
       break;
     case 88:
-      audio.volume = 0;
+      audio.volume = 0.686;
       break;
     case 89:
-      audio.volume = 0;
+      audio.volume = 0.694;
       break;
     case 90:
-      audio.volume = 0;
+      audio.volume = 0.702;
       break;
     case 91:
-      audio.volume = 0;
+      audio.volume = 0.710;
       break;
     case 92:
-      audio.volume = 0;
+      audio.volume = 0.718;
       break;
     case 93:
-      audio.volume = 0;
+      audio.volume = 0.725;
       break;
     case 94:
-      audio.volume = 0;
+      audio.volume = 0.733;
       break;
     case 95:
-      audio.volume = 0;
+      audio.volume = 0.741;
       break;
     case 96:
-      audio.volume = 0;
+      audio.volume = 0.749;
       break;
     case 97:
-      audio.volume = 0;
+      audio.volume = 0.757;
       break;
     case 98:
-      audio.volume = 0;
+      audio.volume = 0.764;
       break;
     case 99:
-      audio.volume = 0;
+      audio.volume = 0.772;
       break;
     case 100:
-      audio.volume = 0;
+      audio.volume = 0.780;
       break;
     case 101:
-      audio.volume = 0;
+      audio.volume = 0.788;
       break;
     case 102:
-      audio.volume = 0;
+      audio.volume = 0.796;
       break;
     case 103:
-      audio.volume = 0;
+      audio.volume = 0.803;
       break;
     case 104:
-      audio.volume = 0;
+      audio.volume = 0.811;
       break;
     case 105:
-      audio.volume = 0;
+      audio.volume = 0.819;
       break;
     case 106:
-      audio.volume = 0;
+      audio.volume = 0.827;
       break;
     case 107:
-      audio.volume = 0;
+      audio.volume = 0.835;
       break;
     case 108:
-      audio.volume = 0;
+      audio.volume = 0.842;
       break;
     case 109:
-      audio.volume = 0;
+      audio.volume = 0.850;
       break;
     case 110:
-      audio.volume = 0;
+      audio.volume = 0.858;
       break;
     case 111:
-      audio.volume = 0;
+      audio.volume = 0.866;
       break;
     case 112:
-      audio.volume = 0;
+      audio.volume = 0.874;
       break;
     case 113:
-      audio.volume = 0;
+      audio.volume = 0.881;
       break;
     case 114:
-      audio.volume = 0;
+      audio.volume = 0.889;
       break;
     case 115:
-      audio.volume = 0;
+      audio.volume = 0.897;
       break;
     case 116:
-      audio.volume = 0;
+      audio.volume = 0.905;
       break;
     case 117:
-      audio.volume = 0;
+      audio.volume = 0.913;
       break;
     case 118:
-      audio.volume = 0;
+      audio.volume = 0.920;
       break;
     case 119:
-      audio.volume = 0;
+      audio.volume = 0.928;
       break;
     case 120:
-      audio.volume = 0;
+      audio.volume = 0.936;
       break;
     case 121:
-      audio.volume = 0;
+      audio.volume = 0.944;
       break;
     case 122:
-      audio.volume = 0;
+      audio.volume = 0.952;
       break;
     case 123:
-      audio.volume = 0;
+      audio.volume = 0.959;
       break;
     case 124:
-      audio.volume = 0;
+      audio.volume = 0.967;
       break;
     case 125:
-      audio.volume = 0;
+      audio.volume = 0.975;
       break;
     case 126:
-      audio.volume = 0;
+      audio.volume = 0.9823;
       break;
     case 127:
-      audio.volume = 0;
+      audio.volume = 1;
       break;
     default:
       console.log("over 10");
