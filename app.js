520edda0db7e246143a5305519f673513b6e3502
Marcus McBride <marcusmcb@gmail.com>
2020-09-16 11:14:34 -0700

    moved setMIDIDevice function back into app.js

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 import { setVelocity } from "./velocities.js";
-import { setMIDIDevice } from "./devices.js";
 
 // global array of DOM audio element key values (QWERTY playback)
 let midiConvertedValues = [65, 83, 68, 70, 71, 72, 74, 75, 76];
@@ -149,6 +148,36 @@ function getMIDIMessage(message) {
       noteOff(note);
       break;
   }
+}
+
+// function to set MIDI device & load corresponding input values (unique to most devices)
+function setMIDIDevice(deviceName) {
+  switch (deviceName) {
+    case "Keystation 49es":
+      midiInputValues = [48, 50, 52, 53, 55, 57, 59, 60, 62];
+      break;
+    case "LKMK3 MIDI": // Launchkey MK3
+    case "Launchkey Mini MK3":
+      midiInputValues = [36, 37, 38, 39, 44, 45, 46, 47, 40];
+      break;
+    case "MPK mini 3": // Akai MPK Mini Mk3
+      midiInputValues = [36, 37, 38, 39, 40, 41, 42, 43];
+      break;
+    case "Arturia BeatStep":
+      midiInputValues = [36, 37, 38, 39, 40, 41, 42, 43, 44];
+      break;
+    case "KOMPLETE KONTROL M32 MIDI":
+      midiInputValues = [41, 43, 45, 47, 48, 50, 52, 53, 55];
+      break;
+    case "Maschine MK3 Ctrl MIDI":
+      midiInputValues = [12, 13, 14, 15, 16, 17, 18, 19, 20];
+      break;
+    default:
+      console.log("No MIDI map found for your device!");
+      break;
+  }
+  // logger to confirm MIDI device set
+  console.log(`MIDI DEVICE SET: ${deviceName}`);
 }
 
 // triggers audio element on MIDI note message/event
