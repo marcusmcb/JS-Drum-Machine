637488e96e00ec89cf39b3fb0cad550dadc6b95b
Marcus McBride <marcusmcb@gmail.com>
2020-10-14 18:51:39 -0700

    iterating through kitBPaths array is not behaving properly

--- app.js
+++ app.js
@@ -6,19 +6,7 @@ let midiConvertedValues = [65, 83, 68, 70, 71, 72, 74, 75, 76]
 // global MIDI vars (set later)
 let midiInputValues
 let tempMIDIDevice
-
-let myArray = []
-
-const urls = ['https://mcb-bucket-js.s3-us-west-1.amazonaws.com/kit_b/boom.wav', 'https://mcb-bucket-js.s3-us-west-1.amazonaws.com/kit_b/clap.wav', 'https://mcb-bucket-js.s3-us-west-1.amazonaws.com/kit_b/snare.wav']
-
-Promise.all(urls.map(url =>
-  fetch(url, {mode: 'cors'})
-  .then(response => response).then(data => {
-    console.log(data.url)
-    myArray.push(data.url)
-  })))
-  
-console.log(myArray)
+let kitBPaths = []
 
 async function fetchItem() {
   // fetch requests
@@ -31,7 +19,7 @@ async function fetchItem() {
   let response2 = await fetch(
     'https://mcb-bucket-js.s3-us-west-1.amazonaws.com/kit_b/snare.wav'
   )
-  
+
   console.log(response.statusText)
   if (response.status === 200) {
     // dummy array
@@ -52,7 +40,7 @@ async function fetchItem() {
 function printURL(url) {
   let newSound = document.getElementById('testAudio')
   newSound.src = url
-  console.log(`New sound: ${newSound.src}`)
+  // console.log(`New sound: ${newSound.src}`)
   playAudio(newSound)
 }
 
@@ -146,17 +134,46 @@ function changeKit() {
   // for (let i = 0; i < audioTags.length; i++) {
   //   console.log(audioTags[i].innerHTML)
   // }
+  if (activeKit === 'kit_b') {
+    const urls = [
+      'https://mcb-bucket-js.s3-us-west-1.amazonaws.com/kit_b/kick.wav',
+      'https://mcb-bucket-js.s3-us-west-1.amazonaws.com/kit_b/hihat.wav',
+      'https://mcb-bucket-js.s3-us-west-1.amazonaws.com/kit_b/clap.wav',
+      'https://mcb-bucket-js.s3-us-west-1.amazonaws.com/kit_b/openhat.wav',
+      'https://mcb-bucket-js.s3-us-west-1.amazonaws.com/kit_b/boom.wav',
+      'https://mcb-bucket-js.s3-us-west-1.amazonaws.com/kit_b/ride.wav',
+      'https://mcb-bucket-js.s3-us-west-1.amazonaws.com/kit_b/snare.wav',
+      'https://mcb-bucket-js.s3-us-west-1.amazonaws.com/kit_b/tom.wav',
+      'https://mcb-bucket-js.s3-us-west-1.amazonaws.com/kit_b/tink.wav',
+    ]
+    Promise.all(
+      urls.map((url) =>
+        fetch(url, { mode: 'cors' })
+          .then((response) => response)
+          .then((data) => {
+            console.log(data.url)
+            kitBPaths.push(data.url)
+          })
+      )
+    )
+    console.log(kitBPaths)
+    updateKitB(audioElements, kitBPaths)    
+  } else {
+    for (let i = 0; i < audioElements.length; i++) {
+      let currentSourcePath = audioElements[i].src
+      // console.log(`CSP - ${currentSourcePath}`)
+      let newSourcePath = currentSourcePath.replace(regex, activeKit)
+      audioElements[i].src = newSourcePath
 
-  for (let i = 0; i < audioElements.length; i++) {
-    let currentSourcePath = audioElements[i].src
-    // console.log(`CSP - ${currentSourcePath}`)
-    let newSourcePath = currentSourcePath.replace(regex, activeKit)
-    audioElements[i].src = newSourcePath
-
-    // logger to pull pad name from source path for dynamic update (future UI)
-    // console.log(newSourcePath.split(`${activeKit}/`)[1].split('.')[0])
-    // audioElements[i].src = audioElements[i].src.split("-Machine/")[1];
+      // logger to pull pad name from source path for dynamic update (future UI)
+      // console.log(newSourcePath.split(`${activeKit}/`)[1].split('.')[0])
+      // audioElements[i].src = audioElements[i].src.split("-Machine/")[1];
+    }
   }
+}
+
+function updateKitB(audioElements, kitBPaths) {
+  console.log("WORKED")
 }
 
 // function to load currently selected kit
