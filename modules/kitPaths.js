f295fbaaa5e90b2fe221774f7466ca0b0017360d
Marcus McBride <marcusmcb@gmail.com>
2020-10-17 10:27:12 -0700

    added S3 code, properly returning file paths as S3 object

--- modules/kitPaths.js
+++ modules/kitPaths.js
@@ -1,58 +1,63 @@
-const kitAPaths = [
-  'https://mcb-bucket-js.s3-us-west-1.amazonaws.com/kit_a/kick.wav',
-  'https://mcb-bucket-js.s3-us-west-1.amazonaws.com/kit_a/hihat.wav',
-  'https://mcb-bucket-js.s3-us-west-1.amazonaws.com/kit_a/clap.wav',
-  'https://mcb-bucket-js.s3-us-west-1.amazonaws.com/kit_a/openhat.wav',
-  'https://mcb-bucket-js.s3-us-west-1.amazonaws.com/kit_a/boom.wav',
-  'https://mcb-bucket-js.s3-us-west-1.amazonaws.com/kit_a/ride.wav',
-  'https://mcb-bucket-js.s3-us-west-1.amazonaws.com/kit_a/snare.wav',
-  'https://mcb-bucket-js.s3-us-west-1.amazonaws.com/kit_a/tom.wav',
-  'https://mcb-bucket-js.s3-us-west-1.amazonaws.com/kit_a/tink.wav',
-]
+// *** S3 SDK config ***
 
-const kitBPaths = [
-  'https://mcb-bucket-js.s3-us-west-1.amazonaws.com/kit_b/kick.wav',
-  'https://mcb-bucket-js.s3-us-west-1.amazonaws.com/kit_b/hihat.wav',
-  'https://mcb-bucket-js.s3-us-west-1.amazonaws.com/kit_b/clap.wav',
-  'https://mcb-bucket-js.s3-us-west-1.amazonaws.com/kit_b/openhat.wav',
-  'https://mcb-bucket-js.s3-us-west-1.amazonaws.com/kit_b/boom.wav',
-  'https://mcb-bucket-js.s3-us-west-1.amazonaws.com/kit_b/ride.wav',
-  'https://mcb-bucket-js.s3-us-west-1.amazonaws.com/kit_b/snare.wav',
-  'https://mcb-bucket-js.s3-us-west-1.amazonaws.com/kit_b/tom.wav',
-  'https://mcb-bucket-js.s3-us-west-1.amazonaws.com/kit_b/tink.wav',
-]
+var s3BucketName = 'jsp-1'
 
-const kitCPaths = [
-  'https://mcb-bucket-js.s3-us-west-1.amazonaws.com/kit_c/kick.wav',
-  'https://mcb-bucket-js.s3-us-west-1.amazonaws.com/kit_c/hihat.wav',
-  'https://mcb-bucket-js.s3-us-west-1.amazonaws.com/kit_c/clap.wav',
-  'https://mcb-bucket-js.s3-us-west-1.amazonaws.com/kit_c/openhat.wav',
-  'https://mcb-bucket-js.s3-us-west-1.amazonaws.com/kit_c/boom.wav',
-  'https://mcb-bucket-js.s3-us-west-1.amazonaws.com/kit_c/ride.wav',
-  'https://mcb-bucket-js.s3-us-west-1.amazonaws.com/kit_c/snare.wav',
-  'https://mcb-bucket-js.s3-us-west-1.amazonaws.com/kit_c/tom.wav',
-  'https://mcb-bucket-js.s3-us-west-1.amazonaws.com/kit_c/tink.wav',
-]
+AWS.config.region = 'us-west-2' // Region
+AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+  IdentityPoolId: 'us-west-2:d8f462c8-e5e1-4b21-a7f4-0ba287bba05f',
+})
 
-function setFilePaths(filepaths, urls) {
-  for (let i = 0; i < filepaths.length; i++) {
-    urls.push(filepaths[i])
-  }
-}
+// create a new S3 service object
+var s3 = new AWS.S3({
+  apiVersion: '2006-03-01',
+  params: { Bucket: s3BucketName },
+})
 
-export function setKitPath(activeKit, urls) {  
-  switch (activeKit) {
-    case 'kit_a':
-      setFilePaths(kitAPaths, urls)
-      break
-    case 'kit_b':
-      setFilePaths(kitBPaths, urls)
-      break
-    case 'kit_c':
-      setFilePaths(kitCPaths, urls)
-      break
-    default:
-      console.log('Not found')
-      break
-  }   
+// main function export
+export function setKitPath(activeKit, urls) {
+  
+  // set var to return array of file paths from S3
+  let filePaths = []
+
+  // add slash to activeKit value for later S3 return comparison
+  let newActiveKit = activeKit + '/'
+  console.log(`NEW ACTIVE KIT: ${newActiveKit}`)
+  
+  // S3 code to list folders in bucket
+  s3.listObjects({ Delimiter: '/' }, function (err, data) {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log(data)      
+      let folders = data.CommonPrefixes.map(function (commonPrefix) {
+        let prefix = commonPrefix.Prefix
+        let folderName = decodeURIComponent(prefix.replace('/', ''))
+        let folderKey = encodeURIComponent(folderName) + '/'
+        
+        // S3 code to list files in folders
+        s3.listObjects({ Prefix: folderKey }, function (err, data) {
+          if (err) {
+            console.log(err)
+          } else {
+            
+            // logic to select S3 folder based on user kit selection and set filepath array accordingly
+            if (data.Prefix === activeKit + '/') {
+
+              // "this" refers to data.Prefix in this instance
+              let href = this.request.httpRequest.endpoint.href
+              let bucketUrl = href + s3BucketName + '/'
+              let soundFiles = data.Contents.map(function (soundFile) {
+                let fileKey = soundFile.Key
+                let fileUrl = bucketUrl + encodeURIComponent(fileKey)
+                console.log(fileUrl)
+                filePaths.push(fileUrl)                
+              })
+            }
+          }
+        })
+      })                 
+    }
+  })
+  console.log(filePaths)
+  console.log(typeof filePaths)
 }
