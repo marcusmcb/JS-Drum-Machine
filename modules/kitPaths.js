77a95c6d90038035d800abf6355527fc0e631b2e
Marcus McBride <marcusmcb@gmail.com>
2020-10-18 14:28:37 -0700

    returning array of objects w/file path & pad no

--- modules/kitPaths.js
+++ modules/kitPaths.js
@@ -13,6 +13,11 @@ var s3 = new AWS.S3({
   params: { Bucket: s3BucketName },
 })
 
+function NewItem(url, pad) {
+  this.url = url
+  this.pad = pad
+}
+
 // *** main function export ***
 
 // returns an array of file paths from S3 to update the DOM
@@ -22,16 +27,14 @@ export function setKitPath(activeKit, audioElements) {
   var filePaths = []
 
   // add slash to activeKit value for later S3 return comparison
-  let newActiveKit = activeKit + '/'  
+  let newActiveKit = activeKit + '/'
 
   // S3 code to list folders in bucket
   s3.listObjects({ Delimiter: '/' }, function (err, data) {
     if (err) {
-
       // future dev: redirect to fallback audio source (GitHub, etc)
       // set as function or re-direct user back to app.js ?
       console.log(err)
-
     } else {
       let folders = data.CommonPrefixes.map(function (commonPrefix) {
         let prefix = commonPrefix.Prefix
@@ -40,56 +43,63 @@ export function setKitPath(activeKit, audioElements) {
 
         // S3 code to list files in folders
         s3.listObjects({ Prefix: folderKey }, function (err, data) {
-          
           // future dev: redirect to fallback audio source (GitHub, etc)
           if (err) {
             console.log(err)
           } else {
-            // logic to select S3 folder based on user kit selection and set filepath array accordingly
+            // logic to select S3 folder based on user kit selection and set file paths
             if (data.Prefix === activeKit + '/') {
               // "this" refers to data.Prefix in this instance
               let href = this.request.httpRequest.endpoint.href
               let bucketURL = href + s3BucketName + '/'
               let soundFiles = data.Contents.map(function (soundFile) {
-                let fileKey = soundFile.Key                                
+                let fileKey = soundFile.Key
                 let fileURL = bucketURL + encodeURIComponent(fileKey)
-                fileURL = fileURL.replace('%2F', '/')
-                filePaths.push(fileURL)
-                
-                // let fileName = fileKey.replace(newActiveKit, '')
-                // console.log(fileName) 
-                
-                // let params = {
-                //   Bucket: s3BucketName,
-                //   Key: fileKey
-                // }
+                fileURL = fileURL.replace('%2F', '/')                
+                let fileName = fileKey.replace(newActiveKit, '')                
 
-                // s3.getObjectTagging(params, function(err, data) {
-                //   if (err) {
-                //     console.log(err)
-                //   } else {
-                //     console.log(data.TagSet[0].Value)
-                //   }
-                // })
-              })
-              filePaths.shift()              
+                let params = {
+                  Bucket: s3BucketName,
+                  Key: fileKey,
+                }
+
+                // code to get pad values from tags in s3 files
+                s3.getObjectTagging(params, function (err, data) {
+                  if (err) {
+                    console.log(err)
+                  } else {                    
+                    if (data.TagSet.length === 0) {                      
+                      return
+                    } else {
+                      let padNo = data.TagSet[0].Value                      
+                      let x = new NewItem(fileURL, padNo)
+                      filePaths.push(x)
+                    }
+                  }
+                })
+              })              
+              
               for (let i = 0; i < audioElements.length; i++) {
                 for (let j = 0; j < filePaths.length; j++) {
-                  if (audioElements[i].src != filePaths[j]) {
-                    audioElements[i].src = filePaths[i]                    
+                  console.log(filePaths[i])
+                  if (audioElements[i].src != filePaths[j]) {                    
+                    audioElements[i].src = filePaths[i]
                   }
                 }
               }
-              
+
               // load audio elements after source URLs are updated
               let kit = document.querySelectorAll('audio')
               for (let i = 0; i < kit.length; i++) {
                 kit[i].load()
-              }              
+              }
             }
           }
         })
       })
     }
-  }) 
+  })
+  console.log(filePaths) 
 }
+
+
